a164af3dc738cea549aa11cdc487d1bd1447244b
Phoenix <x.rhythm@qq.com>
2024-03-14 17:58:35 +0800

    fix: update app key window getter.

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -178,10 +178,10 @@ extension UIApplication {
     var appKeyWindow: UIWindow? {
         if #available(iOS 13.0, *) {
             return UIApplication.shared.connectedScenes
-                .filter { $0.activationState == .foregroundActive }
-                .first(where: { $0 is UIWindowScene })
-                .flatMap({ $0 as? UIWindowScene })?.windows
-                .first(where: \.isKeyWindow)
+                .sorted { $0.activationState.sortPriority < $1.activationState.sortPriority }
+                .compactMap { $0 as? UIWindowScene }
+                .compactMap { $0.windows.first { $0.isKeyWindow } }
+                .first
         } else {
             return UIApplication.shared.keyWindow
         }
@@ -256,5 +256,18 @@ prefix func ! (value: Bool?) -> Bool? {
         return false
     } else {
         return true
+    }
+}
+
+@available(iOS 13.0, *)
+private extension UIScene.ActivationState {
+    var sortPriority: Int {
+        switch self {
+        case .foregroundActive: return 1
+        case .foregroundInactive: return 2
+        case .background: return 3
+        case .unattached: return 4
+        @unknown default: return 5
+        }
     }
 }
