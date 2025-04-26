880b941d34f0576305f8c5d7200e459eb06f1416
phoenix <x.rhythm@qq.com>
2025-04-26 18:51:52 +0800

    update swift-syntax

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.1"),
         .package(url: "https://github.com/reers/SectionReader.git", from: "1.0.0"),
     ],
     targets: [
