1c13243dcace581cce4efbf77e4fb0e36a29ce9d
Phoenix <x.rhythm@qq.com>
2025-05-23 16:29:25 +0800

    Change example app to SPM

--- Example/Pods/Target Support Files/Pods-ReerRouter_Example/Pods-ReerRouter_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReerRouter_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ReerRouter_Example
-@end
---
+++ ReerRouterDemo/ReerRouterDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ReerRouterDemo
+//
+//  Created by phoenix on 2025/5/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+}
+
